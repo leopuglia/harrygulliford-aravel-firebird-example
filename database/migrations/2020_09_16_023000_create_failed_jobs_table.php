5ad60f29b27d898942602a3a92d7bcf51b1b6194
Leonardo Puglia <leo@villadaspedras.com>
2020-09-16 04:19:20 -0300

    Migrations alteradas com sucesso, especificando tipo de ID como increments('id') quando o BD 'e Firebird

--- database/migrations/2020_09_16_023000_create_failed_jobs_table.php
+++ database/migrations/2020_09_16_023000_create_failed_jobs_table.php
@@ -14,7 +14,12 @@ class CreateFailedJobsTable extends Migration
     public function up()
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
+            // ADD_LEO
+            if ($table->engine == 'firebird')
+                $table->increments('id');
+            else
+                $table->id();
+
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
